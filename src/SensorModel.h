b46036a43ba10f28e2b3c596a76e34d3a457a36c
Afton Martin <aftonam@gmail.com>
2016-12-04 04:56:55 -0300

    added implementation for movement correction direction

--- src/SensorModel.h
+++ src/SensorModel.h
@@ -5,9 +5,10 @@
 
 class SensorModel {
   private:
-    Direction current_direction;
+    Direction correction_direction;
     IRsend irsend;
-    int freq_bin[5];
+    static const int BIN_ARRAY_SIZE = 5;
+    int freq_bin[BIN_ARRAY_SIZE];
   public:
     SensorModel();
     ~SensorModel() {}
@@ -19,12 +20,12 @@ class SensorModel {
 };
 
 SensorModel::SensorModel() {
-  current_direction = Direction::LEFT;
+  correction_direction = Direction::FORWARD;
 }
 
 Direction SensorModel::getCorrectionDirection()
 {
-  return current_direction;
+  return correction_direction;
 }
 int SensorModel::getOffset()
 {
@@ -32,22 +33,48 @@ int SensorModel::getOffset()
 }
 void SensorModel::collect(int current_position, unsigned long value)
 {
-  freq_bin[(current_position - 50) / 20]++;
-  Serial.print(freq_bin[0], DEC);
-  Serial.print(" ");
-  Serial.print(freq_bin[1], DEC);
-  Serial.print(" ");
-  Serial.print(freq_bin[2], DEC);
+  if (value == 0x61A030CF) {
+    freq_bin[(current_position - 50) / 20]++;
+  }
+  Serial.print(freq_bin[4], DEC);
   Serial.print(" ");
   Serial.print(freq_bin[3], DEC);
   Serial.print(" ");
-  Serial.print(freq_bin[4], DEC);
+  Serial.print(freq_bin[2], DEC);
+  Serial.print(" ");
+  Serial.print(freq_bin[1], DEC);
+  Serial.print(" ");
+  Serial.print(freq_bin[0], DEC);
   Serial.println(" ");
 
 }
 void SensorModel::compute()
 {
-  //
+  //calculate the bins for each angle sampled
+  int max_num = freq_bin[0];
+  int bin_index = 0;
+  for (int i = 0; i < BIN_ARRAY_SIZE; i++) {
+    if (freq_bin[i] > max_num) {
+      max_num = freq_bin[i];
+      bin_index = i;
+    }
+  }
+  //reset if numbers get too big
+  if (max_num > 5) {
+    for (int i = 0; i < BIN_ARRAY_SIZE; i++) {
+      freq_bin[i] = 0;
+    }
+  }
+  //calculate the correct direction to move
+  if (bin_index < 2) {
+    correction_direction = Direction::RIGHT;
+  }
+  else if (bin_index > 2) {
+    correction_direction = Direction::LEFT;
+  }
+  else {
+    correction_direction = Direction::FORWARD;
+  }
 }
 
 void SensorModel::sendCodes() {
--- src/src.ino
+++ src/src.ino
@@ -37,37 +37,38 @@ void loop() {
 
   //move ir sensor
   //cycle_ir(ir_movement_handle, ir_controller_handle);
+  
   //gather sensor input
   if (irrecv.decode(&results)) {
     sensor_model.collect(ir_movement_handle->getPosition(), results.value);
     irrecv.resume(); // Receive the next value
+
+    //calculate input after recieving
+    sensor_model.compute();
+
+    //if too left, go left
+    if (sensor_model.getCorrectionDirection() == Direction::LEFT) {
+      //byte powr = calculatePower((sensor_model.getOffset()));
+      //wheel_movement->setPower(powr);
+      //wheel_controller->left();
+    }
+
+    //if too right, go right
+    //if(SensorModel.getCorrectionDirection() == Direction.Left);
+    //powr = PowerCalculator.calculatePower((SensorReporter.getOffset())));
+    //wheel_movement.setPower(powr);
+    //wheel_controller->right();
+
+    //if 'centered' go forward
+    //if(SensorModel.getCorrectionDirection() == Direction.Forward);
+    //wheel_controller->forward();
+  } else {
+    //if nothing detected
+    //if(SensorModel.getCorrectionDirection() == Direction.Backward);
+    //wheel_controller->backward();
   }
-
-  //calculate input after recieving
-  sensor_model.compute();
-
-  //if too left, go left
-  if (sensor_model.getCorrectionDirection() == Direction::LEFT) {
-    //byte powr = calculatePower((sensor_model.getOffset()));
-    //wheel_movement->setPower(powr);
-    //wheel_controller->left();
-  }
-
-  //if too right, go right
-  //if(SensorModel.getCorrectionDirection() == Direction.Left);
-  //powr = PowerCalculator.calculatePower((SensorReporter.getOffset())));
-  //wheel_movement.setPower(powr);
-  //wheel_controller->right();
-
-  //if 'centered' go forward
-  //if(SensorModel.getCorrectionDirection() == Direction.Forward);
-  //wheel_controller->forward();
-
-  //if nothing detected
-  //if(SensorModel.getCorrectionDirection() == Direction.Backward);
-  //wheel_controller->backward();
-
-    //send the lejos code on Channel 0 to mimic beacon
+  
+  //send the lejos code on Channel 0 to mimic beacon
   sensor_model.sendCodes();
   irrecv.enableIRIn(); // Re-enable receiver
   delay(500);//must delay or tramples over reciever
@@ -76,6 +77,7 @@ void loop() {
 byte calculatePower(int offset) {
   return 0;
 }
+//move the IR sensor around appropriately
 void cycle_ir(IRServoMovement *&ir_movement, IRServoController *&ir_controller) {
   if (ir_movement->limitReached()) {
     if (ir_movement->getDirection() == Direction::LEFT) {
