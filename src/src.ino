cdca833d61111e8d116cf23fa5fb378293e6ccc3
Afton Martin <aftonam@gmail.com>
2016-12-03 21:32:36 -0300

    moved allocation and declaration to more global scope

--- src/src.ino
+++ src/src.ino
@@ -5,25 +5,27 @@
 #include "IRServo.h"
 #include "IMovement.h"
 
+  IMovement *wm;
+  IController *wc;
+  IMovement *ir;
+  IController *ic;
 void setup() {
   Serial.begin(9600);
+  wm = new WheelMovement();
+  wc = new WheelController(wm);
+  ir = new IRServo();
+  ic = new WheelController(ir);
 }
 void loop() {
   delay(1000);
-  IMovement *wm = new WheelMovement();
-  IController *wc = new WheelController(wm);
+
   wc->left();
   wc->right();
   wc->backward();
   wc->forward();
-  IMovement *ir = new IRServo();
-  IController *ic = new WheelController(ir);
+
   ic->left();
   ic->right();
   ic->backward();
   ic->forward();
-  delete wc;
-  delete wm;
-  delete ir;
-  delete ic;
 }
